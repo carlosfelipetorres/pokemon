5605afc9bcae599c7f05d8ffa54523e5b4341faf
carlostorres <carlos.torres@rappi.com>
2017-04-07 15:47:23 -0500

    Actividad de detalle del pokemon. Configuracion del acelerometro.

---
+++ Pokemon/app/src/main/java/com/pokemon/carlostorres/pokemon/presentation/PokemonDetailActivity.java
@@ -0,0 +1,66 @@
+package com.pokemon.carlostorres.pokemon.presentation;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pokemon.carlostorres.pokemon.R;
+import com.pokemon.carlostorres.pokemon.clients.DiComponent;
+
+public class PokemonDetailActivity extends BaseActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pokemon_detail);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+    }
+
+    @Override
+    protected void injectComponent(DiComponent diComponent) {
+        diComponent.inject(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            getAccelerometer(event);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    private void getAccelerometer(SensorEvent event) {
+        float[] values = event.values;
+        // Movement
+        float x = values[0];
+        float y = values[1];
+        float z = values[2];
+
+        float accelationSquareRoot = (x * x + y * y + z * z) / (SensorManager.GRAVITY_EARTH * SensorManager.GRAVITY_EARTH);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+}
